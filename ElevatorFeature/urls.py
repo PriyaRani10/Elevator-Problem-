997a778c988f4811e2823d650b4206d7487efdd5
PriyaRani10 <priyarani0227@gmail.com>
2023-03-03 16:22:26 +0530

    added url  to list and  post the request for  each elevator under a systm

--- ElevatorFeature/urls.py
+++ ElevatorFeature/urls.py
@@ -8,7 +8,10 @@ urlpatterns=[
     path('elevator-system/<int:id>/list/',ElevatorsList.as_view(), name='elevator-list'),
    
     path('elevator-system/add-new/', CreateElevatorSystem.as_view(),name='add-new-elevator-system'),
+    #  url for each requesting elevator view and update 
+    path('elevator-system/<int:id>/elevator/<int:pk>/detail/', ViewUpdateSingleElevatorDetail.as_view(),name='each-elevator-detail-view-and-update'),
+    # url for view and add new req
+    path('elevator-system/<int:id>/elevator/<int:pk>/add-new-req/', ListCreateElevatorRequest.as_view(),name='add-new-request-and view'),
 
-    path('elevator-system/<int:id>/elevator/<int:pk>/detail/', ViewSingleElevatorDetail.as_view(),name='each-elevator-detail-view'),
 
 ]
--- ElevatorFeature/views.py
+++ ElevatorFeature/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics,viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import ElevatorSystem, Elevator, ElevatorRequest
 from .serializers import ElevatorSystemSerializer, ElevatorSerializer, ElevatorRequestSerializer,ElevatorRequestSerializerAll
@@ -45,10 +46,13 @@ class CreateElevatorSystem(generics.CreateAPIView):
     )
 
 
-class ViewSingleElevatorDetail(generics.RetrieveAPIView):
+class ViewUpdateSingleElevatorDetail(generics.RetrieveUpdateAPIView):
   '''
-  Get  the details of a specific elevator that we want, 
-  given its elevator system and elevator number
+  Getting the details of a specific elevator that we want, 
+  given its elevator system and elevator number.
+  Also,
+  Update details of a specific elevator, 
+  given its elevator system and elevator number in URL
   '''
   serializer_class = ElevatorSerializer
 
@@ -63,6 +67,57 @@ class ViewSingleElevatorDetail(generics.RetrieveAPIView):
 
     return queryset[0]
   
+  #override this  put method by patch for partial update
+  def put(self, request, *args, **kwargs):
+    return self.partial_update(request, *args, **kwargs)
+  
+
+class ListCreateElevatorRequest(generics.ListCreateAPIView):
+  '''
+  Create a new request for a specific elevator, 
+  given its elevator system and elevator number in URL.
+  The inputs of requested and destinatiom floor is sent with
+  the form-data.
+  '''
+  filter_backends = [DjangoFilterBackend]
+  filterset_fields = ['is_active']
+  
+  def get_serializer_class(self, *args, **kwargs):
+    return ElevatorRequestSerializer if self.request.method == 'POST' else ElevatorRequestSerializerAll
+      
+  # Overriding the perform_create method, Parent class of 'CreateAPIView'
+  def perform_create(self, serializer):
+    system_id = self.kwargs['id']
+    elevator_number = self.kwargs['pk']
+
+    elevator_object = Elevator.objects.get(
+      elevator_system__id = system_id,
+      elevator_number = elevator_number
+    )
+    # elevator_object = queryset[0]
+
+    serializer.save(elevator = elevator_object)
+
+
+  '''
+  List all the requests for a given elevator
+  Requests already served can be filtered with is_active
+  parameter set false
+  '''
+  def get_queryset(self):
+    system_id = self.kwargs['id']
+    elevator_number = self.kwargs['pk']
+
+    elevator_object = Elevator.objects.get(
+      elevator_system__id = system_id,
+      elevator_number = elevator_number
+    )
+
+    queryset = ElevatorRequest.objects.filter(elevator = elevator_object)
+    return queryset
+    
+
+
   
 
 
