8ef501788c51bf2d19b20706c4c4b801bdef30ed
PriyaRani10 <priyarani0227@gmail.com>
2023-03-03 13:08:50 +0530

    added url for creating elevator system

--- ElevatorFeature/urls.py
+++ ElevatorFeature/urls.py
@@ -6,4 +6,5 @@ urlpatterns=[
 
     path('elevator-system/list/', ElevatorSystemList.as_view(), name='elevator-system-list'),
     path('elevator-system/<int:id>/list/',ElevatorsList.as_view(), name='elevator-list'),
+    path('elevator-system/add-new/', CreateElevatorSystem.as_view(),name='add-new-system'),
 ]
--- ElevatorFeature/views.py
+++ ElevatorFeature/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from .models import ElevatorSystem, Elevator, ElevatorRequest
 from .serializers import ElevatorSystemSerializer, ElevatorSerializer, ElevatorRequestSerializer,ElevatorRequestSerializerAll
 
+from .create_automatic_elevator import create_elevators
 
 class ElevatorSystemList(generics.ListAPIView):
   '''
@@ -26,5 +27,23 @@ class ElevatorsList(generics.ListAPIView):
     queryset = Elevator.objects.filter(elevator_system__id = system_id)
 
     return queryset
+
+
+class CreateElevatorSystem(generics.CreateAPIView):
+  '''
+  Create a new elevator system.
+  '''
+  serializer_class = ElevatorSystemSerializer
+
+  def perform_create(self, serializer):
+    serializer.save()
+
+    # Created elevators needed for the system. so, imported create_automatic_elevators.py
+    create_elevators(
+      number_of_elevators=serializer.data['number_of_elevators'],
+      system_id=serializer.data['id']
+    )
+  
+  
 
 
